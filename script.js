bd2159f962fd1fa683b104855d01230399d3cff9
Steph Pla <stephpla3@gmail.com>
2018-03-15 11:36:55 -0700

    barebones working app

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log("The script linked properly");
+
+const question = 'Please provide a';
+let neededInput = [
+  'n exclamation',
+  'n adverb',
+  ' noun',
+  ' adjective'
+];
+let madLib = [
+  '! he said ',
+  ' as he jumped into his convertible ',
+  ' and drove off with his ',
+  ' wife.'
+];
+let product = '';
+const button = document.getElementById('button');
+
+function doMadLib() {
+  for (let i = 0; i < neededInput.length; i++) {
+    let answer = prompt(question+neededInput[i]);
+    product += answer + madLib[i];
+  }
+  console.log(product);
+
+  var p = document.createElement("P");
+  var text = document.createTextNode(product);
+  p.appendChild(text);
+  document.getElementById("mad-lib-generated").appendChild(p);
+}
+
+button.addEventListener("click", doMadLib, false);
